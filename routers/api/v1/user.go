6410da6fc2c099e5553b66169cf368e9029e14f6
DK SYC <18637890474@163.com>
2019-05-24 13:47:01 +0800

    参数验证

--- routers/api/v1/user.go
+++ routers/api/v1/user.go
@@ -1,16 +1,36 @@
 package v1
 
 import (
+	"blog/models"
 	"blog/pkg/logging"
 	"github.com/Sirupsen/logrus"
 	"github.com/gin-gonic/gin"
+	"gopkg.in/go-playground/validator.v9"
+	"net/http"
 )
 
 func Test(c *gin.Context) {
 
+	validate := validator.New()
+
+	var user models.User
+	err := c.BindJSON(&user)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "",
+		})
+	}
+
+	err = validate.Struct(user)
+
+	if err != nil {
+		return
+	}
+
 	logging.MsgInfo(logrus.WithField("A", "a"), "This is a test")
 
-	c.JSON(200, gin.H{
-		"message": "test",
+	c.JSON(http.StatusOK, gin.H{
+		"message": "",
 	})
 }
